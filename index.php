37f2d43a1b4c9bcc47f93f9c5cb27acd4f2c7dad
Dryss <drysscark@gmail.com>
2024-02-21 18:10:22 +0100

    testcommit2

--- index.php
+++ index.php
@@ -4,5 +4,6 @@
 <head>
     <title>PHP CRUD</title>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 </html>
